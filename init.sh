95557d18db842fe04ad887f09de96e93b772d95d
Bruno Agostini <brunoagostini@natura.net>
2018-01-10 14:18:20 -0200

    Script now fails if any error occurs
    Details:
      - Improved installation
      - Fixed errors on setup that I found in the process

--- init.sh
+++ init.sh
@@ -1,46 +1,25 @@
 #!/bin/bash
+set -e
 
 # Dependencies
 sudo apt-get update && sudo apt-get upgrade
 sudo apt-get install -y curl
 sudo apt-get install -y software-properties-common
-sudo apt-get -y upgradeudo apt-get install -y python-dev python-pip python3-dev python3-pip
+
+# Install python and pip
+sudo apt-get install -y python-dev python-pip python3-dev python3-pip
 sudo apt-get install -y python-pip python3-pip
 sudo pip install --upgrade pip
-sudo npm install -g eslint_d babel-eslint eslint-config-airbnb eslint-plugin-jsx-a11y eslint-plugin-react eslint-plugin-import eslint-config-standard eslint-plugin-node eslint-plugin-promise eslint-plugin-standard
-sudo npm install -g tern
-sudo apt-get install -y silversearcher-ag
 
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-~/.fzf/install
-export FZF_CTRL_T_COMMAND='ag -g "" `git rev-parse --show-toplevel`'
-
-git clone https://github.com/darold/pgFormatter
-cd pgFormatter
-perl Makefile.PL
-make && sudo make install
-cd -
-
-sudo apt-get install -y luarocks lua-check
+# Install Lua
+sudo apt-get install -y luarocks lua-check rubygems ruby-dev
 sudo luarocks install luacheck
 
-# Install it
-sudo add-apt-repository ppa:neovim-ppa/unstable
-sudo apt-get update
-sudo apt-get install -y neovim
+# Install nvm, node and npm
+touch ~/.bash_profile
+wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
+nvm install node
 
-sudo pip2 install --upgrade neovim
-sudo pip3 install --upgrade neovim
-sudo gem install neovim
-
-# Install vim-plug
-curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
-# Use my configs
-mkdir -p ~/.config/nvim/
-ln -s ~/neovim/init.vim ~/.config/nvim/
-ln -s ~/neovim/colors ~/.config/nvim/
-ln -s ~/neovim/.tern-project ~
-
-echo "alias vim=nvim" >> ~/.bashrc
-echo "Done!"
+sudo reboot
---
+++ init2.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+set -e
+
+# Install integrations..
+
+# Globally install ESLint and Tern stuff
+npm install -g eslint_d babel-eslint eslint-config-airbnb eslint-plugin-jsx-a11y eslint-plugin-react eslint-plugin-import eslint-config-standard eslint-plugin-node eslint-plugin-promise eslint-plugin-standard
+npm install -g tern
+
+# Install ag
+sudo apt-get install -y silversearcher-ag
+
+# Install FZF
+sudo rm -fr ~/.fzf && git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install
+export FZF_CTRL_T_COMMAND='ag -g "" `git rev-parse --show-toplevel`'
+
+# Install pgFormatter
+sudo rm -fr pgFormatter && git clone https://github.com/darold/pgFormatter
+cd pgFormatter
+perl Makefile.PL
+make && sudo make install
+cd -
+
+# Install it
+sudo add-apt-repository ppa:neovim-ppa/unstable
+sudo apt-get update
+sudo apt-get install -y neovim
+
+sudo pip2 install --upgrade neovim
+sudo pip3 install --upgrade neovim
+sudo gem install neovim
+
+# Install vim-plug
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# Use my configs
+mkdir -p ~/.config/nvim/
+ln -s ~/neovim/init.vim ~/.config/nvim/
+ln -s ~/neovim/colors ~/.config/nvim/
+ln -s ~/neovim/.tern-project ~
+
+echo "alias vim=nvim" >> ~/.bashrc
+echo "Done!"
