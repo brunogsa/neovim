6df332dceb51548e33cd9b778fbcd6b9c804e6dc
Bruno Salomao <brunogsa@amazon.com>
2023-08-07 08:23:57 -0300

    add a hotkey hint

--- init.lua
+++ init.lua
@@ -529,26 +529,6 @@ Plug('scrooloose/nerdtree', { on = 'NERDTreeToggle' })
 -- *******
 
 Plug('chentoast/marks.nvim')
--- Default mapping
--- mx              Set mark x
--- 'x              Jumps to mark x
--- m,              Set the next available alphabetical (lowercase) mark
--- m;              Toggle the next available mark at the current line
--- dmx             Delete mark x
--- dm-             Delete all marks on the current line
--- dm<space>       Delete all marks in the current buffer
--- m]              Move to next mark
--- m[              Move to previous mark
--- m:              Preview mark. This will prompt you for a specific mark to
---                 preview; press <cr> to preview the next mark.
---
--- m[0-9]          Add a bookmark from bookmark group[0-9].
--- dm[0-9]         Delete all bookmarks from bookmark group[0-9].
--- m}              Move to the next bookmark having the same type as the bookmark under
---                 the cursor. Works across buffers.
--- m{              Move to the previous bookmark having the same type as the bookmark under
---                 the cursor. Works across buffers.
--- dm=             Delete the bookmark under the cursor.
 
 Plug('ggandor/leap.nvim')
 
@@ -1011,6 +991,7 @@ require('marks').setup({
     toggle = "mT",
     next = "m]",
     prev = "m[",
+    -- 'x  Jumps to mark x. Default on neovim
     delete_line = "md",
     delete_buf = "mD",
     preview = false,
