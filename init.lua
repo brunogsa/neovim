7a0d6d46363150245fd6af8e6ae12b75e7d85ff8
Bruno Salomão <brunoagostini@arcoeducacao.com.br>
2025-05-18 23:14:27 -0300

    remove marks.nvim

--- init.lua
+++ init.lua
@@ -567,8 +567,6 @@ Plug('scrooloose/nerdtree', { on = 'NERDTreeToggle' })
   vim.g.NERDTreeHijackNetrw = 1
 -- *******
 
-Plug('chentoast/marks.nvim')
-
 Plug('mg979/vim-visual-multi')
 -- Configs
   vim.g.VM_default_mappings = 0
@@ -986,57 +984,6 @@ require("ibl").setup({
     show_end = true,
     show_start = true
   }
-})
-
-require('marks').setup({
-  -- which builtin marks to show. default {}
-  builtin_marks = { ".", "<", ">", "^" },
-  -- whether movements cycle back to the beginning/end of buffer. default true
-  cyclic = true,
-  -- whether the shada file is updated after modifying uppercase marks. default false
-  force_write_shada = false,
-  -- how often (in ms) to redraw signs/recompute mark positions. 
-  -- higher values will have better performance but may cause visual lag, 
-  -- while lower values may cause performance penalties. default 150.
-  refresh_interval = 250,
-  -- sign priorities for each type of mark - builtin marks, uppercase marks, lowercase
-  -- marks, and bookmarks.
-  -- can be either a table with all/none of the keys, or a single number, in which case
-  -- the priority applies to all marks.
-  -- default 10.
-  sign_priority = { lower=10, upper=15, builtin=8, bookmark=20 },
-  -- disables mark tracking for specific filetypes. default {}
-  excluded_filetypes = {},
-  -- marks.nvim allows you to configure up to 10 bookmark groups, each with its own
-  -- sign/virttext. Bookmarks can be used to group together positions and quickly move
-  -- across multiple buffers. default sign is '!@#$%^&*()' (from 0 to 9), and
-  -- default virt_text is "".
-  bookmark_0 = {
-    sign = "⚑",
-    virt_text = "hello world",
-    -- explicitly prompt for a virtual line annotation when setting a bookmark from this group.
-    -- defaults to false.
-    annotate = false,
-  },
-  default_mappings = false,
-  -- pass false to disable only this default mapping
-  mappings = {
-    set_next = "mz",
-    set = "ma",
-    toggle = "mT",
-    next = "m]",
-    prev = "m[",
-    -- 'x  Jumps to mark x. Default on neovim
-    delete_line = "md",
-    delete_buf = "mD",
-    preview = false,
-    delete = false,
-    set_bookmark0 = false,
-    delete_bookmark = false,
-    next_bookmark = false,
-    prev_bookmark = false,
-    annotate = false,
-  },
 })
 
 require("chatgpt").setup()
