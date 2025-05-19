dbe726fec05d417a79e711eb74861426953346d1
Bruno Salomão <brunoagostini@arcoeducacao.com.br>
2025-05-19 00:50:29 -0300

    remove useless neomake

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@ brew install python
 pip3 install --upgrade pip --break-system-packages
 pip3 install pynvim --break-system-packages
 pip3 install --upgrade pynvim --break-system-packages
+brew install ruff flake8
 
 # Install Ruby
 brew install ruby
@@ -17,10 +18,15 @@ command curl -sSL https://rvm.io/pkuczynski.asc | gpg --import -
 curl -sSL https://get.rvm.io | bash
 rvm get stable --ruby
 
+# Install Rust
+brew install rustup
+rustup-init
+
 # Install Lua
 brew install lua
 brew install luarocks
 luarocks install luacheck
+cargo install selene
 
 # Install perl
 brew install perl
@@ -36,6 +42,7 @@ sudo npm i -g yarn
 mkdir -p ~/.npm
 sudo npm i -g node-gyp
 sudo chown -R $USER:$GROUP ~/.npm
+npm install -g eslint_d
 
 # Install typescript
 sudo npm i -g typescript
