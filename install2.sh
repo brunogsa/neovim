16fc4d5f5acfcbd50bddc41c313545b837a87d4b
Bruno Salomão <brunoagostini@arcoeducacao.com.br>
2025-05-31 09:06:41 -0300

    add bash linter binary to installer.sh

--- install2.sh
+++ install2.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Globally install Tern and Node stuff
-sudo npm install -g tern prettier @asyncapi/generator @asyncapi/html-template @mermaid-js/mermaid-cli redocly
+sudo npm install -g tern prettier @asyncapi/generator @asyncapi/html-template @mermaid-js/mermaid-cli redocly bash-language-server
 
 # Install pgFormatter
 sudo rm -fr pgFormatter && git clone https://github.com/darold/pgFormatter
